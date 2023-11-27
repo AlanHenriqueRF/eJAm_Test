3dafafe9213a6e783bfdbfd97e1a3fa230c5a5e6
AlanHenriqueRF <alhenrangelfera@gmail.com>
2023-11-26 23:12:54 -0300

    Feat: Navbar complete and responsive

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from "react"
+import styled from "styled-components"
+import NavGreaterThen from "./NavScreenGreater";
+import NavLessThen from "./NavLessThen";
+
+//onClick={()=> alert("navigate('/')")}
+//onClick={()=> alert("navigate('/clients')")}
+
+export default function Navbar() {
+    const [matches, setMatches] = useState(
+        window.matchMedia("(min-width: 481px)").matches
+    )
+    useEffect(() => {
+        window
+            .matchMedia("(min-width: 481px)")
+            .addEventListener('change', e => setMatches(e.matches));
+    }, []);
+
+    return (
+        <>
+            <Navbarstyle>
+
+                {matches ? <NavGreaterThen/>: <NavLessThen/>}
+                
+            </Navbarstyle>
+        </>
+    )
+}
+
+const Navbarstyle = styled.div`
+    display:flex;
+    align-items:center;
+    justify-content:space-evenly;
+    flex-shrink: 0;
+    width: 100vw;
+    height:50px;
+    position:fixed;
+    top:0;
+    background-color: #252F3D;
+    font-family: 'Manrope', sans-serif;
+
+    div{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        svg{
+            cursor: pointer;
+        }
+    }
+
+    h1{
+        color: #FFFFFF;
+        font-size: 12px;
+        font-weight: 500;
+        margin-left: 8px;
+        cursor: pointer;
+    }
+
+    @media (max-width: 480px) {
+        width: calc(100vw - 40px);
+        align-items: center;
+        justify-content:space-between; 
+        height: 22px;
+        padding: 10px 20px;
+    }
+`
